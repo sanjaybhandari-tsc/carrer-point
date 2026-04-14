4d68a016e6ee750ed7080546067a1df183d6560f
nirmalsingh16 <nirmal.singh@techsuperiors.com>
2026-04-14 10:16:22 +0530

    homepage heroSection

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import styles from '@/styles/Home/Home.module.css';
+import styles from "@/styles/Home/Home.module.css";
 import HeroSection from "./HeroSection";
 import FeaturesSection from "./FeatureSection";
 
