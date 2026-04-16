3fe43a45f89b8924898a82efd131baf3a8060bd8
nirmalsingh16 <nirmal.singh@techsuperiors.com>
2026-04-16 20:00:58 +0530

    added in parmanenhiring

---
+++ src/components/PermanentHirings/WorkCard.js
@@ -0,0 +1,13 @@
+export default function WorkCard({ item }) {
+  return (
+    <div className="min-w-[300px] md:min-w-[400px] px-6">
+      <h2 className="text-5xl font-bold text-white/70 mb-4">{item.id}</h2>
+
+      <h3 className="text-xl md:text-2xl font-semibold text-white mb-3">
+        {item.heading}
+      </h3>
+
+      <p className="text-white/80 leading-relaxed">{item.desc}</p>
+    </div>
+  );
+}
--- src/components/PermanentHirings/index.js
+++ src/components/PermanentHirings/index.js
@@ -1,6 +1,8 @@
+import Common from "../Home/Common";
 import HeaderPart from "./HeaderPart";
 import HowWeWork from "./HowWeWork";
 import LongtermTalentHiring from "./LongtermTalentHiring";
+import WeConnect from "./WeConnect";
 import WhychooseUs from "./WhychooseUs";
 
 export default function PermanentHirings() {
@@ -10,6 +12,8 @@ export default function PermanentHirings() {
       <LongtermTalentHiring />
       <WhychooseUs />
       <HowWeWork />
+      <WeConnect />
+      <Common />
     </>
   );
 }
