28f4b1e8ec1120e0152005ca281f4d202d9b86cf
sai07052003 <bonthaharshithsai@gmail.com>
2026-04-29 12:02:01 +0530

    Merge branch 'main' of https://github.com/sanjaybhandari-tsc/carrer-point into SomeCorrections

---
+++ carrer-point/.next/dev/static/chunks/pages/jobs/browse-jobs.js
@@ -0,0 +1,11 @@
+__turbopack_load_page_chunks__("/jobs/browse-jobs", [
+  "static/chunks/node_modules_next_dist_compiled_0o6l_m6._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_0~pg0mt._.js",
+  "static/chunks/node_modules_next_dist_client_0pe1dg-._.js",
+  "static/chunks/node_modules_next_dist_0k8zr~w._.js",
+  "static/chunks/node_modules_react-dom_0bruynb._.js",
+  "static/chunks/node_modules_0lx093h._.js",
+  "static/chunks/[root-of-the-server]__0stn0u.._.js",
+  "static/chunks/[project]_pages_jobs_browse-jobs_index_07xvfw~.js",
+  "static/chunks/turbopack-[project]_pages_jobs_browse-jobs_index_0iltw-q.js"
+])
