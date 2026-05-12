89d45db2ba7042009dbb662f9c58f05e6ade36ce
sai07052003 <bonthaharshithsai@gmail.com>
2026-05-12 15:16:28 +0530

    changes in banner conssitency

--- src/components/commons/HeroText.js
+++ src/components/commons/HeroText.js
@@ -29,53 +29,79 @@ function HeaderPart({ headingText }) {
     //     {headingText}
     //   </h1>
     // </div>
-     <header>
-      <section className="relative flex justify-center items-center bg-gradient-to-b from-[#0277BD] to-[#0E6497] py-10 md:py-14 lg:py-16 mb-10 md:mb-14 lg:mb-16 rounded-b-2xl overflow-hidden">
+    //  <header>
+    //   <section className="relative flex justify-center items-center bg-gradient-to-b from-[#0277BD] to-[#0E6497] py-10 md:py-14 lg:py-16 mb-10 md:mb-14 lg:mb-16 rounded-b-2xl overflow-hidden">
         
-        {/* Decorative Elements */}
-        <Image
-          className="absolute -top-13 lg:-top-10 -left-5"
-          alt=""
-          aria-hidden="true"
-          src="/images/permanentHiring/permanentTop1.svg"
-          width={70}
-          height={70}
-        />
+    //     {/* Decorative Elements */}
+    //     <Image
+    //       className="absolute -top-13 lg:-top-10 -left-5"
+    //       alt=""
+    //       aria-hidden="true"
+    //       src="/images/permanentHiring/permanentTop1.svg"
+    //       width={70}
+    //       height={70}
+    //     />
 
-        <Image
-          className="absolute -top-7 -left-0 lg:top-0 lg:left-0 md:-top-4 md:left-0"
-          alt=""
-          aria-hidden="true"
-          src="/images/permanentHiring/permanentTop2.svg"
-          width={100}
-          height={100}
-        />
+    //     <Image
+    //       className="absolute -top-7 -left-0 lg:top-0 lg:left-0 md:-top-4 md:left-0"
+    //       alt=""
+    //       aria-hidden="true"
+    //       src="/images/permanentHiring/permanentTop2.svg"
+    //       width={100}
+    //       height={100}
+    //     />
 
-        <Image
-          className="absolute -top-7 -right-0 lg:top-0 lg:right-0 md:-top-4 md:right-0"
-          alt=""
-          aria-hidden="true"
-          src="/images/permanentHiring/permanentTop3.svg"
-          width={100}
-          height={100}
-        />
+    //     <Image
+    //       className="absolute -top-7 -right-0 lg:top-0 lg:right-0 md:-top-4 md:right-0"
+    //       alt=""
+    //       aria-hidden="true"
+    //       src="/images/permanentHiring/permanentTop3.svg"
+    //       width={100}
+    //       height={100}
+    //     />
 
-        <Image
-          className="absolute -top-13 lg:-top-10 -right-5"
-          alt=""
-          aria-hidden="true"
-          src="/images/permanentHiring/permanentTop4.svg"
-          width={70}
-          height={70}
-        />
+    //     <Image
+    //       className="absolute -top-13 lg:-top-10 -right-5"
+    //       alt=""
+    //       aria-hidden="true"
+    //       src="/images/permanentHiring/permanentTop4.svg"
+    //       width={70}
+    //       height={70}
+    //     />
 
-        {/* Main Heading */}
-        <h1 className="main-heading  tracking-normal text-white">
-          {headingText}
-        </h1>
+    //     {/* Main Heading */}
+    //     <h1 className="main-heading  tracking-normal text-white">
+    //       {headingText}
+    //     </h1>
 
-      </section>
-    </header>
+    //   </section>
+    // </header>
+    <header className="h-[56px] md:h-[80px] lg:h-[120px] relative flex justify-center items-center bg-gradient-to-b from-[#0277BD] to-[#0E6497] md:py-12 py-10 rounded-b-2xl overflow-hidden" aria-label="Submit CV header">
+            <img
+                className="absolute  lg:top-0 lg:left-0  md:top-0 md:left-0 -top-10 -left-5 "
+                src="/images/permanentHiring/permanentTop1.svg"
+                aria-hidden="true"
+            />
+            <img
+                className="absolute  lg:top-0 lg:left-0  md:top-0 md:left-0 -top-10 -left-5 "
+                src="/images/permanentHiring/permanentTop2.svg"
+                aria-hidden="true"
+            />
+
+            <img
+                className="absolute lg:top-0 lg:right-0 md:top-0 md:right-0  -top-10 -right-5"
+                src="/images/permanentHiring/permanentTop3.svg"
+                aria-hidden="true"
+            />
+            <img
+                className="absolute lg:top-0 lg:right-0 md:top-0 md:right-0  -top-10 -right-5"
+                src="/images/permanentHiring/permanentTop4.svg"
+                aria-hidden="true"
+            />
+            <h2 className="heading-bold text-white">
+                 {headingText}
+            </h2>
+        </header>
   );
 }
 
